902601b9a08ab3463f937263695191b0f163bab4
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-11 11:51:27 +0800

    AndroidRuntime: enable low memory heap mode
    Change-Id: I72ce41d6a67e81211cc7e42f060a29c19a052f55
    Signed-off-by: newestzdn <127198882+newestzdn@users.noreply.github.com>

--- core/jni/AndroidRuntime.cpp
+++ core/jni/AndroidRuntime.cpp
@@ -894,10 +894,7 @@ int AndroidRuntime::startVm(JavaVM** pJavaVM, JNIEnv** pEnv, bool zygote, bool p
     parseRuntimeOption("dalvik.vm.ps-min-first-save-ms", profileMinFirstSaveOptsBuf,
             "-Xps-min-first-save-ms:");
 
-    property_get("ro.config.low_ram", propBuf, "");
-    if (strcmp(propBuf, "true") == 0) {
       addOption("-XX:LowMemoryMode");
-    }
 
     /*
      * Garbage-collection related options.
